64897654226c65eff5abdbed34d462eb846d2ac2
Rens-DV-HAN <RJ.deVreede@student.han.nl>
2025-03-21 09:46:00 +0100

    Fix various issues in Google Maps API prototype script

--- prototypes/GoogleMapsAPIPrototype/script.sh
+++ prototypes/GoogleMapsAPIPrototype/script.sh
@@ -1,7 +1,11 @@
-source .env # Load the RapidAPI key
+#!/bin/sh
+
+source ./.env # Load the RapidAPI key
 
 curl \
+	-H 'Content-Type: application/json' \
 	-H "X-Goog-FieldMask: *" \
+	-H "x-rapidapi-host: google-map-places-new-v2.p.rapidapi.com" \
 	-H "x-rapidapi-key: $RAPIDAPI_KEY" \
 	-d '{"textQuery":"Amsterdam Airport Schiphol"}' \
-    https://google-map-places-new-v2.p.rapidapi.com/v1/places:autocomplete 
+    https://google-map-places-new-v2.p.rapidapi.com/v1/places:searchText
