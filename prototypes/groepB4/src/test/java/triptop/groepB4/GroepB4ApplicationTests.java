11c62876c0e0af304a4ca9629d71fc7a65278613
jordy Eulink <jordy.eulink31@gmail.com>
2025-03-31 13:19:38 +0200

    start applicatie

---
+++ prototypes/groepB4/src/test/java/triptop/groepB4/GroepB4ApplicationTests.java
@@ -0,0 +1,13 @@
+package triptop.groepB4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroepB4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
