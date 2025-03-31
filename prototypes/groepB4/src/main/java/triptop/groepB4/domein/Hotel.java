b85958f8f33172f457081afa4f5516fb6197bda7
jordy Eulink <jordy.eulink31@gmail.com>
2025-03-31 15:32:43 +0200

    meer invulling

--- prototypes/groepB4/src/main/java/triptop/groepB4/domein/Hotel.java
+++ prototypes/groepB4/src/main/java/triptop/groepB4/domein/Hotel.java
@@ -1,4 +1,47 @@
 package triptop.groepB4.domein;
 
+import triptop.groepB4.state.BookingState;
+import triptop.groepB4.state.Gepland;
+
 public class Hotel {
+    private int hotelId;
+    private String hotelName;
+    private BookingState state;
+
+    public Hotel(int hotelId, String hotelName, BookingState state) {
+        this.hotelId = hotelId;
+        this.hotelName = hotelName;
+        this.state = state;
+    }
+
+    public Hotel(int hotelId, String hotelName) {
+        this.hotelId = hotelId;
+        this.hotelName = hotelName;
+        state = new Gepland();
+    }
+
+
+    public int getHotelId() {
+        return hotelId;
+    }
+
+    public void setHotelId(int hotelId) {
+        this.hotelId = hotelId;
+    }
+
+    public String getHotelName() {
+        return hotelName;
+    }
+
+    public void setHotelName(String hotelName) {
+        this.hotelName = hotelName;
+    }
+
+    public BookingState getState() {
+        return state;
+    }
+
+    public void setState(BookingState state) {
+        this.state = state;
+    }
 }
