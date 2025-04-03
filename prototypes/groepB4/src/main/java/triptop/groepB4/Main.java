59693123e0b6fdeeea9ee391bc5511a25100e0cb
Rens-DV-HAN <RJ.deVreede@student.han.nl>
2025-04-03 11:31:46 +0200

    Add SMS verify login functionality

--- prototypes/groepB4/src/main/java/triptop/groepB4/Main.java
+++ prototypes/groepB4/src/main/java/triptop/groepB4/Main.java
@@ -1,12 +1,13 @@
 package triptop.groepB4;
 
-import triptop.groepB4.service.IdentityProviderLoginService;
-import triptop.groepB4.service.IdentityProviderLoginServiceAdapter;
-import triptop.groepB4.service.LoginService;
+import triptop.groepB4.service.*;
 
 public class Main {
     public static void main(String[] args) {
-        LoginService loginService = new IdentityProviderLoginServiceAdapter(new IdentityProviderLoginService("username", "password"));
-        String token = loginService.login();
+        LoginService loginService1 = new IdentityProviderLoginServiceAdapter(new IdentityProviderLoginService("username", "password"));
+        String token1 = loginService1.login();
+
+        LoginService loginService2 = new SmsVerifyLoginServiceAdapter(new SmsVerifyLoginService("abc123"));
+        String token2 = loginService2.login();
     }
 }
